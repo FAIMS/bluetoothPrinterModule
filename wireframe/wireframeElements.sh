34d862ed3cf251ece8314da90e765974a68282e0
Brian Ballsun-Stanton <brian@fedarch.org>
2017-05-30 17:15:25 +1000

    With files

---
+++ wireframe/wireframeElements.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./makeElement.sh style_orientation_orientation "horizontal" input 1 false false false false false .
+./makeElement.sh style_even_layout_weight "1" input 1 false false false false false .
+./makeElement.sh style_large_layout_weight "3" input 1 false false false false false .
+./makeElement.sh Test_Test_connectToBluetooth "connectToBluetooth" button 1 false false false false false .
+./makeElement.sh Test_Test_testText "testText" input 1 false false false false false .
+./makeElement.sh Test_Test_testPrint "testPrint" button 1 false false false false false .
